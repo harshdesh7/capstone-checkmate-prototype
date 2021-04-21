05670da652890b3fd7d62d0b261c662e92cff778
Josef May <josefmay@Josefs-MacBook-Pro.local>
2021-04-21 13:59:10 -0600

    Dish Price HTML Change Functionality, Commentted Code

--- Node/app.js
+++ Node/app.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./Api')
 const app = express();
 const port = process.env.PORT || 5000
 
-// app.get('/', (req, res) => {
-//     // res.sendFile(path.join(__dirname, 'public', 'base-menu.html'));
-//     // console.log('API Key:', api.key);
-// });
-
-
-//Static Folder
+//Static Folder of all HTML pages and functionality
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+//Listens for the start of the app
 app.listen(port, function(err){
     console.log('Server started on port', port);
-    // console.log('API Key:', api.key);
 });
--- Node/public/js/bill-page.js
+++ Node/public/js/bill-page.js
@@ -16,15 +16,12 @@ $(document).ready(function() {
 
     const tax_mult = 0.04;
 
-    // $.getJSON("api.js", function(res){
-    //
-    //     console.log(res);
-    // });
-
-    // $.get("apicall?action=finish&username=dummy&tablename=testaurant-tab1&amount=10.01", function(res){
-    //     console.log(res)
-    // });
-
+    /*
+    Listens for Select all button to be clicked
+    Grab all needed elements on the page
+    Select all the buttons and chnage the price accordingly
+    Returns nothing
+    */
     $('#slct').click(function(e){
         var gknots = document.getElementById("GK");
         var afw1 = document.getElementById("AFW1");
@@ -88,6 +85,13 @@ $(document).ready(function() {
 
     });
 
+    /*
+    Listens for %20 tip button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of tip buttons accordingly
+    Change the price accordingly
+    Returns nothing
+    */
     $('#tip-20').click(function(e){
         if(!button20.classList.contains('option-light-selected-iyMpb6')){
             button20.classList.add('option-light-selected-iyMpb6')
@@ -110,6 +114,13 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for %15 tip button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of tip buttons accordingly
+    Change the price accordingly
+    Returns nothing
+    */
     $('#tip-15').click(function(e){
         if(!button15.classList.contains('option-light-selected-iyMpb6')){
             button15.classList.add('option-light-selected-iyMpb6')
@@ -132,6 +143,13 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for %10 tip button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of tip buttons accordingly
+    Change the price accordingly
+    Returns nothing
+    */
     $('#tip-10').click(function(e){
         if(!button10.classList.contains('option-light-selected-iyMpb6')){
             button10.classList.add('option-light-selected-iyMpb6')
@@ -154,6 +172,14 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the first Alfredo Williams button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of buttons accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#AFW1').click(function(e){
         var afw1 = document.getElementById("AFW1");
         var afw1_RR = document.getElementById("afw1-RR");
@@ -187,6 +213,8 @@ $(document).ready(function() {
             afw1_JM.checked = false;
             afw1_RR.checked = false;
 
+            afw1_price_split(21);
+
             sub_chnge_amt = -21.00/afw1_split
             tax_chnge_amt=sub_chnge_amt*tax_mult
 
@@ -201,6 +229,14 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the second Alfredo Williams button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of buttons accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#AFW2').click(function(e){
         var afw2 = document.getElementById("AFW2");
         var afw2_RR = document.getElementById("afw2-RR");
@@ -235,6 +271,8 @@ $(document).ready(function() {
             afw2_JM.checked = false;
             afw2_RR.checked = false;
 
+            afw2_price_split(21);
+
             sub_chnge_amt = -21.00/afw2_split
             tax_chnge_amt=sub_chnge_amt*tax_mult
 
@@ -250,8 +288,14 @@ $(document).ready(function() {
         }
     });
 
-
-
+    /*
+    Listens for the Garlic Knots button to be clicked
+    Grab all needed elements on the page
+    Change HTML Style of buttons accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#GK').click(function(e){
         var gknots = document.getElementById("GK");
         var gknots_RR = document.getElementById("gknots-RR");
@@ -285,6 +329,8 @@ $(document).ready(function() {
             gknots_JM.checked = false;
             gknots_RR.checked = false;
 
+            gknots_price_split(5);
+
             sub_chnge_amt = -5.00/gknots_split;
             tax_chnge_amt = sub_chnge_amt*tax_mult;
 
@@ -299,6 +345,16 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the first Alfredo Williams split button to be clicked
+    Grab all needed elements on the page
+    Makes sure the user has slected the dish
+    Change HTML Style of tip buttons accordingly
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw1-split').click(function(e){
         var afw1 = document.getElementById("AFW1");
         var menu = document.getElementById("split-menu1");
@@ -316,6 +372,16 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the second Alfredo Williams split button to be clicked
+    Grab all needed elements on the page
+    Makes sure the user has slected the dish
+    Change HTML Style of tip buttons accordingly
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw2-split').click(function(e){
         var afw2 = document.getElementById("AFW2");
         var menu = document.getElementById("split-menu2");
@@ -333,6 +399,16 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the Garlic Knots split button to be clicked
+    Grab all needed elements on the page
+    Makes sure the user has selected the dish
+    Change HTML Style of tip buttons accordingly
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#gknots-split').click(function(e){
         var gknots = document.getElementById("GK");
         var menu = document.getElementById("split-menu3");
@@ -350,8 +426,20 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the Josef May split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw1-JM').click(function(e){
         var afw1_JM = document.getElementById("afw1-JM");
+        var afw1_price = document.getElementById("AFW_price_1");
 
         if (afw1_JM.checked){
             var old_split = -21/afw1_split;
@@ -359,7 +447,10 @@ $(document).ready(function() {
 
             afw1_split=afw1_split+1;
             var new_split = 21/afw1_split;
+            afw1_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
+
+
         }
         else{
             var old_split = -21/afw1_split;
@@ -367,11 +458,23 @@ $(document).ready(function() {
 
             afw1_split=afw1_split-1;
             var new_split = 21/afw1_split;
+            afw1_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
 
     });
 
+    /*
+    Listens for the Josef May split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw2-JM').click(function(e){
         var afw2_JM = document.getElementById("afw2-JM");
 
@@ -381,6 +484,7 @@ $(document).ready(function() {
 
             afw2_split=afw2_split+1;
             var new_split = 21/afw2_split;
+            afw2_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
         else{
@@ -389,11 +493,23 @@ $(document).ready(function() {
 
             afw2_split=afw2_split-1;
             var new_split = 21/afw2_split;
+            afw2_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
 
     });
 
+    /*
+    Listens for the Josef May split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#gknots-JM').click(function(e){
         var gknots_JM = document.getElementById("gknots-JM");
 
@@ -403,6 +519,7 @@ $(document).ready(function() {
 
             gknots_split=gknots_split+1;
             var new_split = 5/gknots_split;
+            gknots_price_split(new_split);
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
         else{
@@ -411,11 +528,23 @@ $(document).ready(function() {
 
             gknots_split=gknots_split-1;
             var new_split = 5/gknots_split;
+            gknots_price_split(new_split);
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
 
     });
 
+    /*
+    Listens for the Rocket Romero split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw1-RR').click(function(e){
         var afw1_RR = document.getElementById("afw1-RR");
 
@@ -425,6 +554,7 @@ $(document).ready(function() {
 
             afw1_split=afw1_split+1;
             var new_split = 21/afw1_split;
+            afw1_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
         else{
@@ -433,10 +563,22 @@ $(document).ready(function() {
 
             afw1_split=afw1_split-1;
             var new_split = 21/afw1_split;
+            afw1_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
     });
 
+    /*
+    Listens for the Rocket Romero split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#afw2-RR').click(function(e){
         var afw2_RR = document.getElementById("afw2-RR");
 
@@ -446,6 +588,7 @@ $(document).ready(function() {
 
             afw2_split=afw2_split+1;
             var new_split = 21/afw2_split;
+            afw2_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
 
         }
@@ -455,10 +598,22 @@ $(document).ready(function() {
 
             afw2_split=afw2_split-1;
             var new_split = 21/afw2_split;
+            afw2_price_split(new_split)
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
     });
 
+    /*
+    Listens for the Rocket Romero split button to be clicked
+    Grab all needed elements on the page
+    Subtracts old splits
+    Adds New split
+    Changes subtotal
+    Change HTML Style of price accordingly
+    Change the price accordingly
+    Adds a split to the dish
+    Returns nothing
+    */
     $('#gknots-RR').click(function(e){
         var gknots_RR = document.getElementById("gknots-RR");
 
@@ -468,6 +623,7 @@ $(document).ready(function() {
 
             gknots_split=gknots_split+1;
             var new_split = 5/gknots_split;
+            gknots_price_split(new_split);
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
         else{
@@ -476,11 +632,18 @@ $(document).ready(function() {
 
             gknots_split=gknots_split-1;
             var new_split = 5/gknots_split;
+            gknots_price_split(new_split);
             tot_amt_no_tip = chge_subtot(new_split, tip);
         }
 
     });
 
+    /*
+    Listens for the confirm payment button to be clicked
+    Grab all needed elements on the page
+    Checks to see if the user has selected any dish to be paid for
+    Returns nothing
+    */
     $('#cfrm-btn').click(function(e){
         var gknots = document.getElementById("GK");
         var afw1 = document.getElementById("AFW1");
@@ -492,18 +655,34 @@ $(document).ready(function() {
         }
     });
 
+    /*
+    Listens for the back button to be clicked
+    Grab all needed elements on the page
+    Checks to see if the user has selected any dish to be paid for
+    Returns nothing
+    */
     $('#back-btn').click(function(e){
         alert('Please finish paying for your dishes before returning to the menu.')
     });
 
 });
 
+/*
+Changes the Total price on the html page
+Grab all needed elements on the page
+Returns nothing
+*/
 function chge_tot(sub_tax, tip){
     var tot_amt = document.getElementById("total-amt");
     var new_tot = (sub_tax+(sub_tax*tip)).toFixed(2);
     tot_amt.innerHTML = new_tot;
 }
 
+/*
+Changes the subtotal price on the html page
+Grab all needed elements on the page
+Returns nothing
+*/
 function chge_subtot(del_sub, tip){
     var sub_amt = document.getElementById('subtotal-amt');
     var tax_amt = document.getElementById('tax-amt');
@@ -522,4 +701,70 @@ function chge_subtot(del_sub, tip){
     tax_amt.innerHTML = tax_num.toFixed(2);
     chge_tot(sub_num+tax_num, tip);
     return (sub_num+tax_num);
+}
+
+/*
+Changes the HTML style of Dish prices
+Grab all needed elements on the page
+Checks if splits are selected
+Returns nothing
+*/
+function afw1_price_split(subdish){
+    var afw1_JM = document.getElementById("afw1-JM");
+    var afw1_RR = document.getElementById("afw1-RR");
+    var afw1_price = document.getElementById("AFW_price_1");
+
+    if(afw1_JM.checked || afw1_RR.checked){
+        afw1_price.style = "color: green;"
+        afw1_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+    if(!afw1_JM.checked && !afw1_RR.checked){
+        afw1_price.style = "color: black;"
+        afw1_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+
+}
+
+/*
+Changes the HTML style of Dish prices
+Grab all needed elements on the page
+Checks if splits are selected
+Returns nothing
+*/
+function afw2_price_split(subdish){
+    var afw2_JM = document.getElementById("afw2-JM");
+    var afw2_RR = document.getElementById("afw2-RR");
+    var afw2_price = document.getElementById("AFW_price_2");
+
+    if(afw2_JM.checked || afw2_RR.checked){
+        afw2_price.style = "color: green;"
+        afw2_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+    if(!afw2_JM.checked && !afw2_RR.checked){
+        afw2_price.style = "color: black;"
+        afw2_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+
+}
+
+/*
+Changes the HTML style of Dish prices
+Grab all needed elements on the page
+Checks if splits are selected
+Returns nothing
+*/
+function gknots_price_split(subdish){
+    var gknots_JM = document.getElementById("gknots-JM");
+    var gknots_RR = document.getElementById("gknots-RR");
+    var gknots_price = document.getElementById("GK_price");
+
+    if(gknots_JM.checked || gknots_RR.checked){
+        gknots_price.style = "color: green;"
+        gknots_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+    if(!gknots_JM.checked && !gknots_RR.checked){
+        gknots_price.style = "color: black;"
+        gknots_price.innerHTML = "$"+subdish.toFixed(2)
+    }
+
 }
